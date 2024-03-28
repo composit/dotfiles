b1756bb34ad7f70ac93f257ee1d836c9fa09f490
Mr. Matt <matt@composition9.com>
2024-03-28 07:24:56 -0600

    shortcuts for copilot chat

--- home/matt/.config/nvim.linkre/lua/copilot.lua
+++ home/matt/.config/nvim.linkre/lua/copilot.lua
@@ -3,3 +3,6 @@ require("CopilotChat").setup {
 }
 
 vim.keymap.set('n', '<leader>cc', ':CopilotChat<cr>', opts)
+vim.keymap.set('v', '<leader>ce', ':CopilotChatExplain<cr>', opts)
+vim.keymap.set('v', '<leader>cf', ':CopilotChatFix<cr>', opts)
+vim.keymap.set('v', '<leader>ct', ':CopilotChatTests<cr>', opts)
