ed9754e12785024495ab12463e7a0e4dd136af31
Mr. Matt <matt@composition9.com>
2025-01-15 15:21:09 -0700

    add lazyvim

---
+++ home/matt/.config/lazyvim.linkre/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  { "aditya-azad/candle-grey" },
+  { "davidosomething/vim-colors-meh" },
+  { "jaredgorski/fogbell.vim" },
+  { "fxn/vim-monochrome" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vim-monochrome",
+      ---colorscheme = "fogbell",
+      ---colorscheme = "candle-grey",
+      ---colorscheme = "meh",
+    },
+  },
+}
