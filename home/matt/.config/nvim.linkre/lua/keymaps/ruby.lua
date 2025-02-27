7317e73c01d1f0a253c65b32396c84584986f400
Mr. Matt <matt@composition9.com>
2025-02-27 15:33:01 -0700

    remove outdated keymap

--- home/matt/.config/nvim.linkre/lua/keymaps/ruby.lua
+++ home/matt/.config/nvim.linkre/lua/keymaps/ruby.lua
@@ -1,6 +1,3 @@
--- Open javascript files
-vim.keymap.set('n', '<leader>j', ':CommandTFlush<CR>|:CommandT app/assets/javascripts<CR>')
-
 -- RUNNING TESTS
 vim.keymap.set('n', '<leader>t', function() RunTestFile() end)
 vim.keymap.set('n', '<leader>a', function() RunTests('') end)
